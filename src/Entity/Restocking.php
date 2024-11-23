8ea13da2bc3247600cb0dba6afbf97250c02b5c5
gome0057 <raphael.gomes1@etudiant.univ-reims.fr>
2024-11-23 10:11:24 +0100

    Entity Restocking (+repository) added

---
+++ src/Entity/Restocking.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RestockingRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RestockingRepository::class)]
+class Restocking
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $restockDate = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $status = null; //Value = "Pending", "Received"
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRestockDate(): ?\DateTimeInterface
+    {
+        return $this->restockDate;
+    }
+
+    public function setRestockDate(?\DateTimeInterface $restockDate): static
+    {
+        $this->restockDate = $restockDate;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}

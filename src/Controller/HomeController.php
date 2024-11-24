b79975dfc295b740d76f9b31423acd8ab8a1d269
Platel-lentz Mathis <mathis.platel-lentz@etudiant.univ-reims.fr>
2024-11-24 20:53:17 +0100

    Adding an action for user to update his account and a comment to describe each route

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,12 +8,12 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/')]
+    #[Route('/')] //Default route
     public function index(): Response
     {
          return $this->redirectToRoute('app_home', [], Response::HTTP_SEE_OTHER);
     }
-    #[Route('/home', name: 'app_home')]
+    #[Route('/home', name: 'app_home')] //Home page route
     public function show(): Response
     {
         return $this->render('home/index.html.twig');
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,20 +10,26 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
+    #[Route('/user', name: 'app_user')] //User connection page route
     public function index(): Response
     {
         return $this->render('user/index.html.twig');
     }
-    #[Route('/user/{id}', name: 'app_user_show', requirements: ['id' => '\d+'])]
+    #[Route('/user/{id}', name: 'app_user_show', requirements: ['id' => '\d+'])] //User account page route
     public function show(): Response
     {
         return $this->render('user/show.html.twig');
     }
-    #[Route('/user/register', name: 'app_user_register')]
+    #[Route('/user/register', name: 'app_user_register')] //User register page route
     public function register(EntityManagerInterface $entityManager,Request $request): Response
     {
         return $this->render('user/register.html.twig');
     }
 
+
+    #[Route('/user/{id}/update', name: 'app_user_update', requirements: ['id' => '\d+'])] //User updating account page route
+    public function update(EntityManagerInterface $entityManager,Request $request): Response
+    {
+        return $this->render('user/update.html.twig');
+    }
 }

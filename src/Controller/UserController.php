8ec7df8d64f9b6c6346ed9502b5c7f53e638d21e
Platel-lentz Mathis <mathis.platel-lentz@etudiant.univ-reims.fr>
2024-11-24 20:43:16 +0100

    Adding an action for user to register

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -16,6 +18,12 @@ class UserController extends AbstractController
     #[Route('/user/{id}', name: 'app_user_show', requirements: ['id' => '\d+'])]
     public function show(): Response
     {
-        return $this->render('user/index.html.twig');
+        return $this->render('user/show.html.twig');
     }
+    #[Route('/user/register', name: 'app_user_register')]
+    public function register(EntityManagerInterface $entityManager,Request $request): Response
+    {
+        return $this->render('user/register.html.twig');
+    }
+
 }

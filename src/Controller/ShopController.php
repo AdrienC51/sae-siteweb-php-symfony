55306bf90fa255e9d43d6d24c20e9b0e6a114f1a
Platel-lentz Mathis <mathis.platel-lentz@etudiant.univ-reims.fr>
2024-11-25 17:21:36 +0100

    Shop controllers for the shop page

---
+++ src/Controller/ShopController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ShopController extends AbstractController
+{
+    #[Route('/shop', name: 'app_shop')]
+    public function index(): Response
+    {
+        return $this->render('shop/index.html.twig');
+    }
+    #[Route('/shop/cart', name: 'app_shop')]
+    public function show(): Response
+    {
+        return $this->render('shop/show.html.twig');
+    }
+}

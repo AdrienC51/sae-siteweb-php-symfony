db32c6c8536ddfc0d23474172cc8259f1e0a0ff4
gome0057 <raphael.gomes1@etudiant.univ-reims.fr>
2024-11-25 22:25:27 +0100

    fix repository reference

--- src/Entity/Unit.php
+++ src/Entity/Unit.php
@@ -2,11 +2,11 @@
 
 namespace App\Entity;
 
-use App\Repository\ArticleRepository;
+use App\Repository\UnitRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+#[ORM\Entity(repositoryClass: UnitRepository::class)]
 class Unit
 {
     #[ORM\Id]

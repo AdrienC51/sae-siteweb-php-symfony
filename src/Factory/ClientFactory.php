dc979c1cfe07a8de6b614534cfdbf375eea4b3da
Platel-lentz Mathis <mathis.platel-lentz@etudiant.univ-reims.fr>
2024-12-09 20:05:36 +0100

    adding Client Facotry

---
+++ src/Factory/ClientFactory.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Client;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<Client>
+ */
+final class ClientFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return Client::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'account' => null, // TODO add App\Entity\Account type manually
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Client $client): void {})
+        ;
+    }
+}

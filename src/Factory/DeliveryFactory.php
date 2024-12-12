72468022dda07f0c367d1fa38f35de3e70c00e2e
Platel-lentz Mathis <mathis.platel-lentz@etudiant.univ-reims.fr>
2024-12-12 17:00:57 +0100

    fixtures and factory Delivery files

---
+++ src/Factory/DeliveryFactory.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Delivery;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<Delivery>
+ */
+final class DeliveryFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return Delivery::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Delivery $delivery): void {})
+        ;
+    }
+}

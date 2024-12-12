7da3298750aba43404ad94181e5f8624500621e0
Platel-lentz Mathis <mathis.platel-lentz@etudiant.univ-reims.fr>
2024-12-12 18:04:18 +0100

    Fixtures and factory of Order files

---
+++ src/Factory/OrderFactory.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Order;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<Order>
+ */
+final class OrderFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return Order::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'client' => ClientFactory::new(),
+            'destAddress' => self::faker()->text(200),
+            'destCity' => self::faker()->text(128),
+            'destPostCode' => self::faker()->text(5),
+            'status' => self::faker()->text(20),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Order $order): void {})
+        ;
+    }
+}

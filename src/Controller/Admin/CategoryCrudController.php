53c3d4693022e140ae62856ceea00bce1608c338
Raphael Gomes <raphael.gomes1@etudiant.univ-reims.fr>
2024-12-21 20:17:45 +0100

    change to delete an article's category when it is delete from the Category CRUD

--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Category;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
@@ -12,6 +14,12 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class CategoryCrudController extends AbstractCrudController
 {
+    private ArticleRepository $articleRepository;
+
+    public function __construct(ArticleRepository $articleRepository)
+    {
+        $this->articleRepository = $articleRepository;
+    }
     public static function getEntityFqcn(): string
     {
         return Category::class;
@@ -30,4 +38,45 @@ class CategoryCrudController extends AbstractCrudController
         ];
     }
 
+
+public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+{
+    $this->setUpdateArticles($entityInstance);
+    parent::updateEntity($entityManager, $entityInstance);
+    $entityManager->flush();
+}
+
+public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+{
+    $this->setUpdateArticles($entityInstance);
+    parent::persistEntity($entityManager, $entityInstance);
+    $entityManager->flush();
+
+}
+
+public function setUpdateArticles(Category $category): void
+{
+    $allArticles = $this->articleRepository->findAll();
+
+    if (isset($this->getContext()->getRequest()->get("Category")['articles'])) {
+        $articlesId = $this->getContext()->getRequest()->get('Category')['articles'];
+
+        foreach ($allArticles as $article) {
+            if ($article->getCategories()->contains($category)) {
+                if (!in_array($article->getId(), $articlesId)) {
+                    $article->removeCategory($category);
+                }
+            } elseif (in_array($article->getId(), $articlesId)) {
+                $article->addCategory($category);
+            }
+        }
+    } else {
+        foreach ($allArticles as $article) {
+            if ($article->getCategories()->contains($category)) {
+                $article->removeCategory($category);
+            }
+        }
+
+    }
+}
 }

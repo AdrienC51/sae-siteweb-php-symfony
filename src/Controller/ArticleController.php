3752f2bae69b24a4c41df958a7ce31c8ba6d9973
Grazex24 <mathis.platel-lentz@etudiant.univ-reims.fr>
2025-01-05 01:05:34 +0100

    Update action : OK

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -18,22 +21,31 @@ class ArticleController extends AbstractController
     #[Route('/article/create', name: 'app_article_create')]
     public function create(): Response
     {
+
         return $this->render('article/create.html.twig', [
             'controller_name' => 'ArticleController',
         ]);
     }
-    #[Route('/article/{id}/update', name: 'app_article_update')]
-    public function update(): Response
+    #[Route('/article/{id}/update', name: 'app_article_update',requirements: ['id' => '\d+'])]
+    public function update(Article $article,Request $request,EntityManagerInterface $entityManager): Response
     {
-        return $this->render('article/update.html.twig', [
-            'controller_name' => 'ArticleController',
-        ]);
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_article_show', ['id' => $article->getId()]);
+        }
+
+        return $this->render('article/update.html.twig', ['article'=>$article,'form'=>$form]);
     }
-    #[Route('/article/{id}/delete', name: 'app_article_delete')]
+    #[Route('/article/{id}/delete', name: 'app_article_delete',requirements: ['id' => '\d+'])]
     public function delete(): Response
     {
         return $this->render('article/delete.html.twig', [
             'controller_name' => 'ArticleController',
         ]);
     }
+
 }

01569fd56f3b0a101dc36fdb63fbb84abaa57547
Grazex24 <mathis.platel-lentz@etudiant.univ-reims.fr>
2025-01-05 01:54:24 +0100

    Delete action : OK

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -7,6 +7,7 @@ use App\Form\ArticleType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -41,11 +42,27 @@ class ArticleController extends AbstractController
         return $this->render('article/update.html.twig', ['article'=>$article,'form'=>$form]);
     }
     #[Route('/article/{id}/delete', name: 'app_article_delete',requirements: ['id' => '\d+'])]
-    public function delete(): Response
+    public function delete(Article $article,Request $request,EntityManagerInterface $entityManager): Response
     {
-        return $this->render('article/delete.html.twig', [
-            'controller_name' => 'ArticleController',
-        ]);
+        $form = $this->createFormBuilder($article)
+            ->add('delete', SubmitType::class)
+            ->add('cancel', SubmitType::class)
+            ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->getClickedButton() === $form->get('delete')) {
+
+                $entityManager->remove($article);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('app_home');
+
+            } else {
+
+                return $this->redirectToRoute('app_article_show', ['id' => $article->getId()]);
+            }
+        }
+        return $this->render('article/delete.html.twig', ['article' => $article, 'form' => $form]);
     }
 
 }

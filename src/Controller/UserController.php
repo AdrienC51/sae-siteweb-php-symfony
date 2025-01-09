e7460bd22e51d3d0ded308152767fe509fa6b839
leco0129 <tom.lecourt-humbert@etudiant.univ-reims.fr>
2025-01-09 14:08:42 +0100

    Connection user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,13 +7,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')] // User connection page route
-    public function index(): Response
+    #[Route(path: '/user', name: 'app_user')] // User connection page route
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('user/show.html.twig');
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('user/index.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     #[Route('/user/{id}', name: 'app_user_show', requirements: ['id' => '\d+'])] // User account page route
@@ -32,5 +38,11 @@ class UserController extends AbstractController
     public function update(EntityManagerInterface $entityManager, Request $request): Response
     {
         return $this->render('user/update.html.twig');
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
--- src/DataFixtures/AccountFixtures.php
+++ src/DataFixtures/AccountFixtures.php
@@ -10,6 +10,10 @@ class AccountFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        AccountFactory::createOne(['firstname' => 'Tony', 'lastname' => 'Stark', 'email' => 'root@example.com', 'roles' => ['ROLE_ADMIN']]);
+        AccountFactory::createOne(['firstname' => 'Peter', 'lastname' => 'Parker', 'email' => 'user@example.com', 'roles' => ['ROLE_USER']]);
         AccountFactory::createMany(150);
+
+        $manager->flush();
     }
 }

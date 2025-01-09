f4a7b737d61878b101731a1ad8471273712c8bd1
Adrien Cadalen <adrien.cadalen@etudiant.univ-reims.fr>
2025-01-09 02:06:09 +0100

    First version of the stock page

--- src/Controller/StockController.php
+++ src/Controller/StockController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 
 class StockController extends AbstractController
 {
     #[Route('/stock', name: 'app_stock')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository, #[MapQueryParameter] string $search = ''): Response
     {
-        return $this->render('stock/show.html.twig', [
-            'controller_name' => 'StockController',
+        $recherche = $search;
+        $articles = $articleRepository->search($search);
+
+        return $this->render('stock/index.html.twig', [
+            'articles' => $articles,
+            'search' => $search,
         ]);
     }
 }

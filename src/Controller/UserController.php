47e450eee5603669e0f96780aef4a8b0c0bb0e89
leco0129 <tom.lecourt-humbert@etudiant.univ-reims.fr>
2025-01-09 19:27:21 +0100

    Create a new account

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Account;
+use App\Form\AccountType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,7 +23,21 @@ class UserController extends AbstractController
     #[Route('/user/register', name: 'app_user_register')] // User register page route
     public function register(EntityManagerInterface $entityManager, Request $request): Response
     {
-        return $this->render('user/register.html.twig');
+        $account = new Account();
+        $form = $this->createForm(AccountType::class, $account);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $account = $form->getData();
+            $entityManager->persist($account);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_user_show', ['id' => $account->getId()]);
+        }
+
+        return $this->render('user/register.html.twig', [
+            'account' => $account,
+            'form' => $form,
+        ]);
     }
 
     #[Route('/user/{id}/update', name: 'app_user_update', requirements: ['id' => '\d+'])] // User updating account page route

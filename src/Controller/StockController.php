721402abee3f1c290c9bbcb59002e927a66f2059
Adrien Cadalen <adrien.cadalen@etudiant.univ-reims.fr>
2025-01-09 15:32:11 +0100

    Added price filters and translated search bar for stock

--- src/Controller/StockController.php
+++ src/Controller/StockController.php
@@ -11,14 +11,16 @@ use Symfony\Component\Routing\Attribute\Route;
 class StockController extends AbstractController
 {
     #[Route('/stock', name: 'app_stock')]
-    public function index(ArticleRepository $articleRepository, #[MapQueryParameter] string $search = ''): Response
+    public function index(ArticleRepository $articleRepository, #[MapQueryParameter] string $search = '', #[MapQueryParameter] string $prix_min = '', #[MapQueryParameter] string $prix_max = ''): Response
     {
         $recherche = $search;
-        $articles = $articleRepository->search($search);
+        $articles = $articleRepository->search($search, $prix_min, $prix_max);
 
         return $this->render('stock/index.html.twig', [
             'articles' => $articles,
             'search' => $search,
+            'prix_min' => $prix_min,
+            'prix_max' => $prix_max,
         ]);
     }
 }

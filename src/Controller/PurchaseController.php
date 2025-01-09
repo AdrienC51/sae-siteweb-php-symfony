723b12a4b996cea2b8600201004e23834e5cffe1
balez <theo.balez@etudiant.univ-reims.fr>
2025-01-09 11:12:09 +0100

    add field verification

--- src/Controller/PurchaseController.php
+++ src/Controller/PurchaseController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -14,6 +15,22 @@ class PurchaseController extends AbstractController
         return $this->render('purchase/index.html.twig', [
             'controller_name' => 'PurchaseController',
         ]);
+    }
+
+    #[\Symfony\Component\Routing\Annotation\Route('/purchase/confirm', name: 'app_purchase_confirm', methods: ['POST'])]
+    public function confirm(Request $request): Response
+    {
+        $cardNumber = $request->request->get('cardNumber');
+        $expiryDate = $request->request->get('expiryDate');
+        $cvv = $request->request->get('cvv');
+        $cardHolder = $request->request->get('cardHolder');
+
+        if (empty($cardNumber) || empty($expiryDate) || empty($cvv) || empty($cardHolder)) {
+            $this->addFlash('error', 'All fields must be completed.');
+            return $this->redirectToRoute('app_purchase');
+        }
+
+        return $this->redirectToRoute('app_purchase_success');
     }
 
     #[Route('/purchase/success', name: 'app_purchase_success')]

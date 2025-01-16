f0dc70bdf9d30076004dbdcb358d9a26dcaf1961
Raphael Gomes <raphael.gomes1@etudiant.univ-reims.fr>
2025-01-16 11:23:30 +0100

    method detail of StockCOntroller modified to add new Unit in database

--- src/Controller/StockController.php
+++ src/Controller/StockController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Unit;
+use App\Form\UnitType;
 use App\Repository\ArticleRepository;
 use App\Repository\StockEvolutionRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
@@ -26,9 +30,19 @@ class StockController extends AbstractController
         ]);
     }
     #[Route('/stock/{id}', name: 'app_stock_detail', requirements: ['id' => '\d+'])]
-    public function detail(Article $article, StockEvolutionRepository $SERepository): Response
+    public function detail(Article $article, StockEvolutionRepository $SERepository, EntityManagerInterface $entityManager, Request $request): Response
     {
         $stockEvolutions = $SERepository->findByArticleIdOrderedByDate($article->getId());
-        return $this->render('stock/detail.html.twig', ["article"=>$article,"se"=>$stockEvolutions]);
+        $unit = new Unit();
+        $form = $this->createForm(UnitType::class, $unit);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $unit->setArticle($article);
+            $unit->setEntryDate(new \DateTime('now'));
+            $entityManager->persist($unit);
+            $entityManager->flush();
+        }
+        return $this->render('stock/detail.html.twig', ["article"=>$article,"se"=>$stockEvolutions,'form'=>$form]);
     }
 }

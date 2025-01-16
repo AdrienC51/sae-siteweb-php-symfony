28f58402411dc71300075bdd8bc62990ce4aed73
Raphael Gomes <raphael.gomes1@etudiant.univ-reims.fr>
2025-01-16 09:57:33 +0100

    Restocking controller restricted to admin only

--- src/Controller/RestockingController.php
+++ src/Controller/RestockingController.php
@@ -7,9 +7,12 @@ use App\Repository\RestockingRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_ADMIN')]
 class RestockingController extends AbstractController
 {
+
     #[Route('/restocking', name: 'app_restocking')]
     public function index(RestockingRepository $restockingRepository): Response
     {

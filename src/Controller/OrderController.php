3bb6efbaa635e961912c66c91337a1735341de04
Raphael Gomes <raphael.gomes1@etudiant.univ-reims.fr>
2025-01-17 15:46:30 +0100

    Order Controller account restriction added

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -7,15 +7,19 @@ use App\Repository\OrderRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class OrderController extends AbstractController
 {
+    #[IsGranted('ROLE_ADMIN')]
     #[Route('/order', name: 'app_order')]
     public function index(OrderRepository $orderRepository): Response
     {
         $orders = $orderRepository->findAllOrderedByNewestDate();
         return $this->render('order/index.html.twig', ['orders' => $orders]);
     }
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+
     #[Route('/order/{id}', name: 'app_order_show',requirements: ['id' => '\d+'])]
     public function show(Order $order): Response
     {

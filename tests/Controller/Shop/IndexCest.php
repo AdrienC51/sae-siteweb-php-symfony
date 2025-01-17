3907f68c6abbce73c9229854828dbb6e827c4a4b
Adrien Cadalen <adrien.cadalen@etudiant.univ-reims.fr>
2025-01-17 23:17:59 +0100

    Added test for clicking on an article

--- tests/Controller/Shop/IndexCest.php
+++ tests/Controller/Shop/IndexCest.php
@@ -15,4 +15,18 @@ class IndexCest
         $I->seeInTitle('Shop');
         $I->seeNumberOfElements('div.article_shop', 5);
     }
+
+    public function clickOnArticle(ControllerTester $I): void
+    {
+        ArticleFactory::createOne(
+            [
+                'name' => 'Doliprane 500mg',
+            ],
+        );
+        ArticleFactory::createMany(5);
+        $I->amOnPage('/shop');
+        $I->click('Doliprane 500mg');
+        $I->seeResponseCodeIsSuccessful();
+        $I->amOnPage('/article/1');
+    }
 }

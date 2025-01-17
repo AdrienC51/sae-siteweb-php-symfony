c9884ef0e0bf41531ec9c027c80ca1fc65c03673
leco0129 <tom.lecourt-humbert@etudiant.univ-reims.fr>
2025-01-18 00:05:45 +0100

    Some adjusts

--- tests/Controller/User/ShowCest.php
+++ tests/Controller/User/ShowCest.php
@@ -15,19 +15,23 @@ class ShowCest
         $account = AccountFactory::createOne(['firstname' => 'Morgan', 'lastname' => 'Freeman', 'email' => 'free@example.com', 'roles' => ['ROLE_USER']]);
         AccountFactory::createMany(5);
         $realUser = $account->_real();
+
         $I->amLoggedInAs($realUser);
         $I->amOnPage('/home');
         $I->click('account_circleAccount');
         $I->seeResponseCodeIsSuccessful();
         $I->seeCurrentRouteIs('app_user_show', ['id' => $account->getId()]);
+        $I->seeInTitle('Freeman Morgan');
         $I->see('Freeman Morgan', 'h1');
         $I->see('Freeman', 'li');
+        $I->see('You don\'t have any client information (you never ordered)', 'li');
     }
 
     public function adminLinksHere(ControllerTester $I)
     {
         $account = AccountFactory::createOne(['firstname' => 'Apollo', 'lastname' => 'Justice', 'email' => 'just@example.com', 'roles' => ['ROLE_ADMIN']]);
         $realAdmin = $account->_real();
+
         $I->amLoggedInAs($realAdmin);
         $I->amOnPage('/user/1');
         $I->see('Admin Links', 'h2');
@@ -38,6 +42,7 @@ class ShowCest
         $account = AccountFactory::createOne();
         AccountFactory::createOne();
         $realUser = $account->_real();
+
         $I->amLoggedInAs($realUser);
         $I->amOnPage('/user/2');
         $I->see('You should not be able to see these informations !', 'div');
@@ -46,6 +51,7 @@ class ShowCest
     public function noAccessWhenDisconnected(ControllerTester $I)
     {
         AccountFactory::createOne();
+
         $I->amOnPage('/user/1');
         $I->see('You are not even connected !', 'div');
     }
@@ -55,6 +61,7 @@ class ShowCest
         $account = AccountFactory::createOne();
         ClientFactory::createOne(['account' => $account, 'address' => '53 Rue Saint Martin', 'post_code' => '51100', 'city' => 'Reims', 'phone' => '951-677-8834']);
         $realUser = $account->_real();
+
         $I->amLoggedInAs($realUser);
         $I->amOnPage('/user/1');
         $I->see('Orders', 'h2');
--- tests/Controller/User/UpdateCest.php
+++ tests/Controller/User/UpdateCest.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Tests\Controller\User;
 
 use App\Factory\AccountFactory;
@@ -12,13 +11,12 @@ class UpdateCest
     {
         $account = AccountFactory::createOne(['firstname' => 'Maya', 'lastname' => 'Fey', 'email' => 'fey@example.com', 'roles' => ['ROLE_USER']]);
         $realUser = $account->_real();
-        $I->amLoggedInAs($realUser);
 
+        $I->amLoggedInAs($realUser);
         $I->amOnPage('/user/1');
         $I->click('Modify');
         $I->seeResponseCodeIsSuccessful();
         $I->seeCurrentRouteIs('app_user_update', ['id' => $account->getId()]);
-
         $I->seeInTitle('Edition of Fey, Maya');
         $I->see('Edition of Fey, Maya', 'h1');
         $I->see('Modify', 'button');
@@ -29,6 +27,7 @@ class UpdateCest
         $account = AccountFactory::createOne();
         AccountFactory::createOne();
         $realUser = $account->_real();
+
         $I->amLoggedInAs($realUser);
         $I->amOnPage('/user/2/update');
         $I->see('You should not be able to modify this account !', 'div');
@@ -37,6 +36,7 @@ class UpdateCest
     public function noAccessWhenDisconnected(ControllerTester $I)
     {
         AccountFactory::createOne();
+
         $I->amOnPage('/user/1/update');
         $I->see('You are not even connected !', 'div');
     }

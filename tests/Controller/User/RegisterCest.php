0b8f474b33fcaaada4b59d227a40b21826263075
leco0129 <tom.lecourt-humbert@etudiant.univ-reims.fr>
2025-01-17 23:38:33 +0100

    user/register test form

---
+++ tests/Controller/User/RegisterCest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Controller\User;
+
+use App\Tests\Support\ControllerTester;
+
+class RegisterCest
+{
+    public function form(ControllerTester $I)
+    {
+        $I->amOnPage('/login');
+        $I->click('You don\'t have an account ?');
+        $I->seeInTitle('Make your new account !');
+        $I->see('Make your new account !', 'h1');
+        $I->see('Create', 'button');
+    }
+}

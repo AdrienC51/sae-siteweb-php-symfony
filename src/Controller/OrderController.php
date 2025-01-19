cebb038ffa912ae82cbbadfb7dabedd8cae743e1
bits0002 <emmanuel.bitsindou@etudiant.univ-reims.fr>
2025-01-19 17:16:45 +0100

    Page complet

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -3,7 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Order;
+use App\Entity\OrderLine;
+use App\Entity\StockEvolution;
 use App\Repository\OrderRepository;
+use App\Repository\ClientRepository;
+use App\Repository\CartLineRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -25,4 +30,112 @@ class OrderController extends AbstractController
     {
         return $this->render('order/show.html.twig', ['order' => $order]);
     }
+
+    #[Route('/order/create/{clientId}', name: 'app_order_create')]
+    public function create(
+        int $clientId,
+        ClientRepository $clientRepository,
+        CartLineRepository $cartLineRepository,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $client = $clientRepository->find($clientId);
+        
+        if (!$client) {
+            throw $this->createNotFoundException('Client non trouvé');
+        }
+        
+        $cartLines = $cartLineRepository->findBy(['client' => $client]);
+        foreach ($cartLines as $cartLine) {
+            $article = $cartLine->getArticle();
+            $quantityRequested = $cartLine->getQuantity();
+            $availableQuantity = $article->getAvailableQuantity();
+            
+            if ($quantityRequested > $availableQuantity) {
+                $this->addFlash('error', sprintf(
+                    'Stock insuffisant pour l\'article: %s (Quantité disponible: %d)',
+                    $article->getName(),
+                    $availableQuantity
+                ));
+                return $this->redirectToRoute('app_cart', ['clientId' => $clientId]);
+            }
+        }
+        
+        $order = new Order();
+        $order->setOrderDate(new \DateTime());
+        $order->setClient($client);
+        $order->setStatus('Pending'); 
+        $order->setDestAddress($client->getAddress());
+        $order->setDestPostCode($client->getPostCode());
+        $order->setDestCity($client->getCity());
+        
+        foreach ($cartLines as $cartLine) {
+            $orderLine = new OrderLine();
+            $orderLine->setArticle($cartLine->getArticle());
+            $orderLine->setQuantity($cartLine->getQuantity());
+            $orderLine->setRelatedOrder($order);
+            
+            $entityManager->persist($orderLine);
+        }
+        
+        $entityManager->persist($order);
+        $entityManager->flush();
+        
+        return $this->redirectToRoute('app_order_confirmation', ['orderId' => $order->getId()]);
+    }
+
+    #[Route('/order/confirmation/{orderId}', name: 'app_order_confirmation')]
+    public function confirmation(
+        int $orderId, 
+        OrderRepository $orderRepository
+    ): Response {
+        $order = $orderRepository->find($orderId);
+        
+        if (!$order) {
+            throw $this->createNotFoundException('Commande non trouvée');
+        }
+        
+        $total = 0;
+        foreach ($order->getOrderLines() as $line) {
+            $total += $line->getQuantity() * $line->getArticle()->getPrice();
+        }
+        
+        return $this->render('order/com.html.twig', [
+            'order' => $order,
+            'total' => $total
+        ]);
+    }
+
+    #[Route('/order/payment/{orderId}', name: 'app_order_payment')]
+    public function payment(
+        int $orderId, 
+        OrderRepository $orderRepository,
+        CartLineRepository $cartLineRepository,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $order = $orderRepository->find($orderId);
+        
+        if (!$order) {
+            throw $this->createNotFoundException('Commande non trouvée');
+        }
+
+        $cartLines = $cartLineRepository->findBy(['client' => $order->getClient()]);
+        
+        foreach ($cartLines as $cartLine) {
+            $stockEvolution = new StockEvolution();
+            $stockEvolution->setArticle($cartLine->getArticle());
+            $stockEvolution->setQuantity($cartLine->getQuantity());
+            $stockEvolution->setType('OUT');
+            $stockEvolution->setEvolutionDate(new \DateTime());
+            
+            $entityManager->persist($stockEvolution);
+            $entityManager->remove($cartLine);
+        }
+
+        $order->setStatus('Accepted');
+        
+        $entityManager->flush();
+        
+        return $this->redirectToRoute('app_payment_success', ['orderId' => $order->getId()]);
+    }
+
 }
